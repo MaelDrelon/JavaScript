cb624e965e678909ecf6892e7b7d78f0cddde3ab
MaelDrelon <mael.drelon1@gmail.com>
2021-01-29 11:08:02 +0100

    upd

--- Défi.js
+++ Défi.js
@@ -6,16 +6,16 @@ var timerActif = false;
 if(document.images)
 {
   chiffre = new Array(10);
-  chiffre[0] = new Image(); chiffre[0].src = "images/cyan/c0.gif";
-  chiffre[1] = new Image(); chiffre[1].src = "images/cyan/c1.gif";
-  chiffre[2] = new Image(); chiffre[2].src = "images/cyan/c2.gif";
-  chiffre[3] = new Image(); chiffre[3].src = "images/cyan/c3.gif";
-  chiffre[4] = new Image(); chiffre[4].src = "images/cyan/c4.gif";
-  chiffre[5] = new Image(); chiffre[5].src = "images/cyan/c5.gif";
-  chiffre[6] = new Image(); chiffre[6].src = "images/cyan/c6.gif";
-  chiffre[7] = new Image(); chiffre[7].src = "images/cyan/c7.gif";
-  chiffre[8] = new Image(); chiffre[8].src = "images/cyan/c8.gif";
-  chiffre[9] = new Image(); chiffre[9].src = "images/cyan/c9.gif";
+  chiffre[0] = new Image(); chiffre[0].src = "images/dark green/dg0.gif";
+  chiffre[1] = new Image(); chiffre[1].src = "images/dark green/dg1.gif";
+  chiffre[2] = new Image(); chiffre[2].src = "images/dark green/dg2.gif";
+  chiffre[3] = new Image(); chiffre[3].src = "images/dark green/dg3.gif";
+  chiffre[4] = new Image(); chiffre[4].src = "images/dark green/dg4.gif";
+  chiffre[5] = new Image(); chiffre[5].src = "images/dark green/dg5.gif";
+  chiffre[6] = new Image(); chiffre[6].src = "images/dark green/dg6.gif";
+  chiffre[7] = new Image(); chiffre[7].src = "images/dark green/dg7.gif";
+  chiffre[8] = new Image(); chiffre[8].src = "images/dark green/dg8.gif";
+  chiffre[9] = new Image(); chiffre[9].src = "images/dark green/dg9.gif";
   Blanc = new Image(); Blanc.src = "images/red/rblanc.gif";
 }
 function Arréter() 
